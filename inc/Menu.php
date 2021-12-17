076db03dfaccea6af9effd2037e7813be5f2a7d1
thimarsola <tmarsola94@gmail.com>
2021-12-17 11:15:24 -0300

    conclusion hero section

---
+++ inc/Menu.php
@@ -0,0 +1,16 @@
+<?php
+
+//REGISTER MENU
+function register_my_menus()
+{
+    register_nav_menus([
+        //Home
+        'header-home' => __('Home Menu'),
+        'footer-home' => __('Home Footer Menu'),
+        //Pages
+        'header-menu' => __('Header Menu'),
+        'footer-menu' => __('Footer Menu'),
+    ]);
+}
+
+add_action('init', 'register_my_menus');
---
+++ template-parts/home/home-hero.php
@@ -0,0 +1,25 @@
+<section class="hero d-flex jc-center ai-center t-center">
+    <div class="container">
+
+        <!-- header -->
+         <header class="hero__header mb-4">
+             <p class="clr-primary-500 mb-3 f-3"><strong>Soluções em Segurança do Trabalho e Meio Ambiente</strong></p>
+             <h2 class="clr-white-500 f-8">Locação de Equipamentos e Instrumentos de Medição</h2>
+         </header>
+        <!-- end of header -->
+
+        <!-- body -->
+         <div class="hero__body">
+             <p class="mb-4 clr-white-500"><strong>Há mais de 20 anos oferecendo soluções em segurança do trabalho e Meio Ambiente</strong></p>
+
+             <!-- cta -->
+              <div class="hero__body__cta d-flex jc-center gp-4">
+                  <a href="<?= get_the_permalink(get_page_by_title('Produtos')) ?>" class="btn btn-white-500" title="Conheça a nossa linha de produtos">Nossos produtos</a>
+                  <a href="#quem-somos" class="btn btn-primary-500" title="Conheça um pouco mais sobre nós">Quem somos</a>
+              </div>
+             <!-- end of cta -->
+         </div>
+        <!-- end of body -->
+
+    </div>
+</section>
