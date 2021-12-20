97462957fa173cec4aa51f77f11f1b3177c2e293
thimarsola <tmarsola94@gmail.com>
2021-12-20 08:20:58 -0300

    conclusion categories section

--- home.php
+++ home.php
@@ -4,41 +4,9 @@
 <?php get_template_part('template-parts/home/home', 'hero'); ?>
 <!-- end of hero -->
 
-<!-- about -->
-<?php get_template_part('template-parts/home/home', 'about'); ?>
-<!-- end of about -->
-
-<!-- gallery -->
-<?php get_template_part('template-parts/home/home', 'gallery'); ?>
-<!-- end of gallery -->
-
-<!-- treatments -->
-<?php get_template_part('template-parts/home/home', 'treatments'); ?>
-<!-- end of treatments -->
-
-<!-- technologies -->
-<?php get_template_part('template-parts/home/home', 'technologies'); ?>
-<!-- end of technologies -->
-
-<!-- team -->
-<?php get_template_part('template-parts/home/home', 'team'); ?>
-<!-- end of team -->
-
-<!-- divider -->
-<?php get_template_part('template-parts/home/home', 'divider'); ?>
-<!-- end of divider -->
-
-<!-- partners -->
-<?php get_template_part('template-parts/home/home', 'partners'); ?>
-<!-- end of partners -->
-
-<!-- contact -->
-<?php get_template_part('template-parts/home/home', 'contact'); ?>
-<!-- end of contact -->
-
-<!-- blog -->
-<?php get_template_part('template-parts/home/home', 'blog'); ?>
-<!-- end of blog -->
+<!-- categories -->
+<?php get_template_part('template-parts/home/home', 'categories'); ?>
+<!-- end of categories -->
 
 
 <?php get_footer(); ?>
---
+++ template-parts/home/home-categories.php
@@ -0,0 +1,118 @@
+<section class="categories py-s">
+    <div class="container">
+
+        <!-- header -->
+         <header class="categories__header mb-6 t-center">
+             <p class="clr-primary-500 f-3 mb-3">Os melhores Equipamentos e Instrumentos de Medição</p>
+             <h2 class="f-8">O Equipamento que você precisa, de acordo com a sua necessidade!</h2>
+         </header>
+        <!-- end of header -->
+
+        <!-- row -->
+         <div class="categories__row grid">
+             <!-- card -->
+              <article class="categories__row__card bg-white-500 shadow">
+                  <!-- thumb -->
+                   <div class="categories__row__card__thumb">
+                       <?= picture('imagem-seguranca-do-trabalho', 'Conheça a nossa linha de equipamentos para segurança do trabalho', 'img-fluid'); ?>
+                   </div>
+                  <!-- end of thumb -->
+
+                  <!-- content -->
+                   <div class="categories__row__card__content p-4">
+                       <!-- header -->
+                        <header class="categories__row__card__content__header mb-2">
+                            <img src="<?= image('safety.svg', 'svg'); ?>" alt="Conheça a nossa linha de equipamentos para segurança do trabalho">
+                            <h3 class="f-3 mt-4">Segurança do Trabalho</h3>
+                        </header>
+                       <!-- end of header -->
+
+                       <!-- body -->
+                        <div class="categories__row__card__content__body">
+                            <p class="mb-2">Conheça a nossa linha de equipamentos para segurança do trabalho.</p>
+                            <a href="<?= get_the_permalink(get_page_by_title('Segurança do Trabalho')); ?>" class="link-neutral-500" title="Conheça a nossa linha de equipamento para Segurança do Trabalho">
+                                <span class="d-flex ai-center">
+                                    Saiba mais
+                                    <i class="icon-arrow-right ml-1"></i>
+                                </span>
+                            </a>
+                        </div>
+                       <!-- end of body -->
+
+                   </div>
+                  <!-- end of content -->
+              </article>
+             <!-- end of card -->
+
+             <!-- card -->
+              <article class="categories__row__card bg-white-500 shadow">
+                  <!-- thumb -->
+                   <div class="categories__row__card__thumb">
+                       <?= picture('imagem-meio-ambiente', 'Conheça a nossa linha de equipamentos para meio ambiente', 'img-fluid'); ?>
+                   </div>
+                  <!-- end of thumb -->
+
+                  <!-- content -->
+                   <div class="categories__row__card__content p-4">
+                       <!-- header -->
+                        <header class="categories__row__card__content__header mb-2">
+                            <img src="<?= image('environment.svg', 'svg'); ?>" alt="Conheça a nossa linha de equipamentos para meio ambiente">
+                            <h3 class="f-3 mt-4">Meio Ambiente</h3>
+                        </header>
+                       <!-- end of header -->
+
+                       <!-- body -->
+                        <div class="categories__row__card__content__body">
+                            <p class="mb-2">Conheça a nossa linha de equipamentos para segurança do trabalho.</p>
+                            <a href="<?= get_the_permalink(get_page_by_title('Meio Ambiente')); ?>" class="link-neutral-500" title="Conheça a nossa linha de equipamento para Segurança do Trabalho">
+                                <span class="d-flex ai-center">
+                                    Saiba mais
+                                    <i class="icon-arrow-right ml-1"></i>
+                                </span>
+                            </a>
+                        </div>
+                       <!-- end of body -->
+
+                   </div>
+                  <!-- end of content -->
+              </article>
+             <!-- end of card -->
+
+             <!-- card -->
+              <article class="categories__row__card bg-white-500 shadow">
+                  <!-- thumb -->
+                   <div class="categories__row__card__thumb">
+                       <?= picture('imagem-outros', 'Conheça os nossos diversos equipamentos para monitoramento e análises.', 'img-fluid'); ?>
+                   </div>
+                  <!-- end of thumb -->
+
+                  <!-- content -->
+                   <div class="categories__row__card__content p-4">
+                       <!-- header -->
+                        <header class="categories__row__card__content__header mb-2">
+                            <img src="<?= image('others.svg', 'svg'); ?>" alt="Conheça os nossos diversos equipamentos para monitoramento e análises.">
+                            <h3 class="f-3 mt-4">Outros</h3>
+                        </header>
+                       <!-- end of header -->
+
+                       <!-- body -->
+                        <div class="categories__row__card__content__body">
+                            <p class="mb-2">Conheça os nossos diversos equipamentos para monitoramento e análises.</p>
+                            <a href="<?= get_the_permalink(get_page_by_title('Outros')); ?>" class="link-neutral-500" title="Conheça a nossa linha de equipamento para Segurança do Trabalho">
+                                <span class="d-flex ai-center">
+                                    Saiba mais
+                                    <i class="icon-arrow-right ml-1"></i>
+                                </span>
+                            </a>
+                        </div>
+                       <!-- end of body -->
+
+                   </div>
+                  <!-- end of content -->
+              </article>
+             <!-- end of card -->
+         </div>
+        <!-- end of row -->
+
+    </div>
+</section>
